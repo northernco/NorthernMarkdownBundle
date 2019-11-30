c810a2c4560ba1e3a560056c1eb1888323812bc7
Nate Wiebe <nate@northern.co>
2019-11-30 13:43:25 -0500

    Add parser service

---
+++ DependencyInjection/NorthernMarkdownExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Northern\MarkdownBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class NorthernMarkdownExtension extends \Symfony\Component\HttpKernel\DependencyInjection\Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
