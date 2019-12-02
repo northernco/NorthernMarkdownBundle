287c23e29141f16d05ccea4d93863ee197d18daa
Nate Wiebe <nate@northern.co>
2019-12-02 10:04:05 -0500

    Make extension lazy load

--- Twig/Extension/MarkdownExtension.php
+++ Twig/Extension/MarkdownExtension.php
@@ -4,22 +4,10 @@ namespace Northern\MarkdownBundle\Twig\Extension;
 
 class MarkdownExtension extends \Twig\Extension\AbstractExtension
 {
-    private $markdownRepository;
-
-    public function __construct(\Northern\MarkdownBundle\Service\MarkdownRepositoryInterface $markdownRepository)
-    {
-        $this->markdownRepository = $markdownRepository;
-    }
-
     public function getFilters(): array
     {
         return [
-            new \Twig\TwigFilter('md2html', [$this, 'markdownToHtml'], ['is_safe' => ['html']]),
+            new \Twig\TwigFilter('md2html', [MarkdownRuntime::class, 'markdownToHtml'], ['is_safe' => ['html']]),
         ];
-    }
-
-    public function markdownToHtml(string $text): string
-    {
-        return $this->markdownRepository->getHtmlFromMarkdown($text);
     }
 }
